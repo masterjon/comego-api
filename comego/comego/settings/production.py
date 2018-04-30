3addc7686b7767d80720c010a06eda41e2a83f4a
Jonathan Horta <jonabc7@gmail.com>
2018-04-29 23:17:56 -0500

    production settings

--- comego/comego/settings/production.py
+++ comego/comego/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': get_secret("DATABASE_NAME"),
         'USER': get_secret("DATABASE_USER"),
         'PASSWORD': get_secret("DATABASE_PASSWORD"),
